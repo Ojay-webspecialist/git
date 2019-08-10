d84caa1fdcceae668d0bd58e8c0dc8a32a57eef8
Agene Sunday <sundayojimagene@gmail.com>
2019-08-10 13:44:00 +0100

    second commit

---
+++ public/index.js
@@ -0,0 +1,12 @@
+const btn=document.querySelector('.btn');
+const input =document.querySelector('.name');
+const display =document.querySelector('.display');
+
+btn.addEventListener('click', show) ;
+
+const show =()=>{
+    console.log('hello there');
+}
+
+
+
